e34e0e3afee72678dcde1af789f8e796edd25607
gokulgk232200 <dgokulgk@gmail.com>
2023-08-04 22:40:26 +0530

    new

---
+++ main.js
@@ -0,0 +1,124 @@
+
+  // STRING METHODS
+  
+  //1) length
+  
+  // var text="first and last"
+  // console.log(text.length);
+  
+  // 2) indexOf
+  
+  // var sum=text.indexOf("last")
+  // console.log(sum);
+  
+  // 3) replace
+  
+  // var sum1=text.replace("and","or")
+  // console.log(sum1);
+  
+  // 4) uppercase
+  
+  // var sum2=text.toUpperCase("");
+  // console.log(sum2);
+  
+  // 5) lowercase
+  
+  // var sum3=text.toLowerCase("");
+  // console.log(sum3);
+  
+  // 6) charAt
+  
+  // var sum4=text.charAt (8);
+  // console.log(sum4);
+   
+   // 7) concat
+   
+   // var sum5=text.concat("GOKULGK");
+  // console.log(sum5);
+  
+  // 8) lastIndexOf
+  
+  // var sum6=text.lastIndexOf ("last");
+  // console.log(sum6);
+   
+  // 9) split
+  
+  // var sum7=text.split("");
+  // console.log(sum7);
+   
+   // 10) tostring
+   
+   // var sum8=text.toString ();
+  // console.log(sum8);
+  
+  // 11) valueOf
+  
+  // var sum9=text.split();
+  // console.log(sum9);
+   
+   // NUMBER METHODS
+   
+  //1) tostring
+  
+  // let a="1";
+  // let b='7';
+  // let num=a+b;
+  // let val=num.toString();
+  // console.log(num);
+  
+  // tofixed
+  // let num = 8.834;
+  // let val = num.toFixed(0);
+  // console.log(val);
+  
+  // valueOf
+  
+  // let num=55;
+  // console.log(num.valueOf());
+  
+  // Boolean method
+  
+  // tostring
+  // let a = "l";
+  // let i  =Boolean(a.toString());
+  // console.log(i);
+  
+  // valueOf
+  
+  // let x=12;
+  // let y=8;
+  // let z=Boolean(x>y.valueOf());
+  
+  // console.log(z);
+  
+  // DATE METHODS
+  
+  // Date
+	const d = new Date();
+	console.log(d);
+ // Day
+	let date = d.getDate();
+	console.log(date);
+	let day = d.getDay()
+	console.log(day);
+	// Year
+	let year = d.getFullYear();
+	console.log(year);
+	// Hours
+	let hour = d.getHours();
+	console.log(hour);
+	// Milliseconds
+	let mill = d.getMilliseconds();
+	console.log(mill);
+	// Minutes
+	let min = d.getMinutes();
+	console.log(min);
+	// month
+	let mon = d.getMonth();
+	console.log(mon);
+	// Sec
+	let sec = d.getSeconds();
+	console.log(sec);
+  
+  
+   
